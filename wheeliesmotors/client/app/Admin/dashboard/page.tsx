7e75f30ad69abc39828878faaa4c13c27d205bac
Ahmed-Boukhallout <massoubouka5@gmail.com>
2024-03-07 21:38:12 +0100

    design

--- wheeliesmotors/client/app/Admin/dashboard/page.tsx
+++ wheeliesmotors/client/app/Admin/dashboard/page.tsx
@@ -11,79 +11,10 @@ import TrendingDownIcon from '@mui/icons-material/TrendingDown';
 import MovingIcon from '@mui/icons-material/Moving';
 import Navbar from "../navbar/page"
 import { AreaChart, Area,BarChart, XAxis, YAxis, Bar, Tooltip, Legend} from "recharts";
-import { PieChart, Pie} from "recharts";
-import styles from"./feedback.module.css"
+
 
 
 const dashboard: React.FC  = () => {
-  const mockTransactions = [
-    {
-      txId: "01",
-      user: "johndoe",
-      date: "2021-09-01",
-      comment: "good job",
-    },
-    {
-      txId: "02",
-      user: "jackdower",
-      date: "2022-04-01",
-     comment: "i like the reservation",
-    },
-    {
-      txId: "03",
-      user: "aberdohnny",
-      date: "2021-09-01",
-      comment: "great work",
-    },
-    {
-      txId: "04",
-      user: "goodmanave",
-      date: "2022-11-05",
-      comment: "awesome",
-    },
-    {
-      txId: "05",
-      user: "stevebower",
-      date: "2022-11-02",
-      comment: "good work",
-    },
-    {
-      txId: "06",
-      user: "aberdohnny",
-      date: "2021-09-01",
-      comment: "good work",
-    },
-    {
-      txId: "07",
-      user: "wootzifer",
-      date: "2019-04-15",
-      comment: "good work",
-    },
-    {
-      txId: "08",
-      user: "jackdower",
-      date: "2022-04-01",
-      comment: "good work",
-    },
-  ];
-  const data01 = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-    { name: "Group E", value: 278 },
-    { name: "Group F", value: 189 }
-  ];
-  
-  const data02 = [
-    { name: "Group A", value: 2400 },
-    { name: "Group B", value: 4567 },
-    { name: "Group C", value: 1398 },
-    { name: "Group D", value: 9800 },
-    { name: "Group E", value: 3908 },
-    { name: "Group F", value: 4800 }
-  ];
-
   const data = [
     {
       "year": "2016",
@@ -162,15 +93,12 @@ const dashboard: React.FC  = () => {
 
 
   return (
-    <div className="flex h-screen"> 
+    <div > 
    <Sidebar/>
-
-      {/* Main Content */}
- 
-      <div className="flex-1 bg-gray-100 p-4 ml-[300px]">
+      <div className="flex-1 bg-gray-100 p-0 ">
     
       <div><Navbar/></div>
-      <Typography variant="h2" fontWeight="bold" style={{ color: '#000001' }}>
+      <Typography variant="h2" fontWeight="bold" style={{ color: '#000001',    marginLeft: '697px' }}>
          Welcome Ahmed 
       </Typography>
       
@@ -180,7 +108,6 @@ const dashboard: React.FC  = () => {
         </Typography>
 
         <div className="grid grid-cols-4 gap-6" >
-          {/* Item Sales */}
           <Box className="p-4 bg-white rounded-md shadow-md transition transform hover:scale-105" >
           <ShoppingCartOutlinedIcon color="primary" />
             <div className="flex items-center">
@@ -198,7 +125,6 @@ const dashboard: React.FC  = () => {
         </Typography>
         </Box>
           </Box>
-
           {/* New Orders */}
           <Box className="p-4 bg-white rounded-md shadow-md transition transform hover:scale-105">
           <PersonAddOutlinedIcon color="primary"/>
@@ -250,15 +176,15 @@ const dashboard: React.FC  = () => {
         </Box>
 
 <div>
-<div style={{ display: 'flex',  alignItems: 'center' }} >
-  <Typography variant="h5" style={{ color: '#000080' , whiteSpace: 'nowrap' }}>Sales Activities</Typography>
-  <Typography variant="h5" style={{ color: '#000080' ,  marginLeft: '400px',  whiteSpace: 'nowrap'}} >New accout by Month</Typography>
+<div style={{ display: 'flex' ,position:"absolute" }} >
+  <Typography variant="h5" style={{ color: '#000080' , whiteSpace: 'nowrap',marginLeft: '330px' }}>Sales Activities</Typography>
+  <Typography variant="h5" style={{ color: '#000080' ,  marginLeft: '753px',  whiteSpace: 'nowrap'}} >New accout by Month</Typography>
 </div>
 
 <div className="mt-[80px]">
 <section className="my-4 px-4 ml-[-100px] ">
   <div className="w-[150px] h-[150px] rounded">
-      <AreaChart width={600} height={230} data={data} 
+      <AreaChart width={600} height={230} style={{marginLeft:"220px"}} data={data} 
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
@@ -280,7 +206,7 @@ const dashboard: React.FC  = () => {
       </section>
       </div>
       <div>
-      <section className=" my-4 px-40 ml-[350px] mt-[-170px]">
+      <section className=" my-4 px-40 ml-[897px] mt-[-170px] ">
   <div className="w-[200px] h-[200px] bg-gray-700 rounded">
     
   <BarChart width={600} height={250} data={bar}>
@@ -295,88 +221,6 @@ const dashboard: React.FC  = () => {
   </div>
 </section>
 </div>
-<div className={styles.cont} >
-            <Box
-            className=" rounded-md shadow-md transition transform hover:scale-10 h-[350px] w-[400px] mt-[60px]"
-          gridColumn="span 4"
-          gridRow="span 2"
-        
-          overflow="auto">
-
-<Box
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            borderBottom={`4px solid `}
-            className="bg-blue"
-            p="15px"
-          >
-            <Typography variant="h5" fontWeight="600">
-              Recent Feedbacks
-            </Typography>
-          </Box>
-          {mockTransactions.map((transaction, i) => (
-            <Box
-              key={`${transaction.txId}-${i}`}
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              borderBottom={`4px solid `}
-              p="15px"
-              overflow={"hidden"}
-            >
-              <Box>
-                <Typography
-              
-                  variant="h5"
-                  fontWeight="600"
-                >
-                  {transaction.txId}
-                </Typography>
-                <Typography 
-                className="text-blue">
-                  {transaction.user}
-                </Typography>
-              </Box>
-              <Box >{transaction.date}</Box>
-              <Box
-               className="text-blue"
-                p="5px 10px"
-                borderRadius="4px"
-              >
-                {transaction.comment}
-              </Box>
-            </Box>
-          ))}
-          </Box>
-        </div>
-<div className="ml-[450px] " style={{ marginTop: '-400px' }}>
-<Typography variant="h5" style={{ color: '#000080' , whiteSpace: 'nowrap', marginLeft: '100px' }}>Analystics</Typography>
-<div className={styles.pye}>
-<PieChart width={1000} height={400} >
-      <Pie
-        dataKey="value"
-        isAnimationActive={false}
-        data={data01}
-        cx={200}
-        cy={200}
-        outerRadius={80}
-        fill="#8884d8"
-        label
-      />
-            <Pie
-        dataKey="value"
-        data={data02}
-        cx={500}
-        cy={200}
-        innerRadius={40}
-        outerRadius={80}
-        fill="#82ca9d"
-      />
-      <Tooltip />
-    </PieChart>
-    </div>
-    </div>
 </div>
 
         </div>
